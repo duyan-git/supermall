dd5f405593ca30223dc785d68b4571eb4bbf6e5c
duyan-git <1031101641@qq.com>
2020-06-03 22:29:28 +0800

    首页推荐

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,6 +2,7 @@
     <div id="home">
         <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
         <home-swiper :banners="banners"></home-swiper>
+        <recommend-view :recommends="recommends"/>
     </div>
 </template>
 
@@ -9,12 +10,14 @@
 import NavBar from '@/components/common/navbar/NavBar'
 import HomeSwiper from './childComps/HomeSwiper'
 import {getHomeMultidata} from '@/network/home'
+import RecommendView from './childComps/RecommendView'
 
 export default {
     name:'Home',
     components:{
         NavBar,
-        HomeSwiper
+        HomeSwiper,
+        RecommendView
     },
     data(){
         return{
@@ -26,7 +29,8 @@ export default {
     created(){
         getHomeMultidata().then(res=>{
             this.result=res;
-            console.log(res.data.data.banner)
+            console.log(res.data.data.banner.list)
+            console.log(res.data.data.recommend.list)
             this.banners=res.data.data.banner.list;
             this.recommends=res.data.data.recommend.list;
         })
